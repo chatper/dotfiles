8767eab9f8d60f97887b8d8742d720bee49dfac7
chatper <peris2525@gmail.com>
2018-09-15 16:39:51 +0300

    Update gmail script

--- config/polybar/gmail.sh
+++ config/polybar/gmail.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -s https://PASSWORD:EMAIL/mail/u/0/feed/atom | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g'
+curl -s https://USERNAME:PASSWORD@mail.google.com/mail/u/0/feed/atom | sed -e 's,.*<fullcount>\([^<]*\)</fullcount>.*,\1,g'
